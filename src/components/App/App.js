4ac8718d3b2220d0fb7bc3174871adf21084461b
Iren Galygina <galigina01@yandex.ru>
2022-11-20 19:24:31 +0300

    Этап 4: Функциональность на React и JS

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -146,6 +146,7 @@ function App() {
         })
         .catch((err) => console.log(err))
     }
+// eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   const handleUpdateProfile = (userData) => {
--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -36,6 +36,7 @@ function SavedMovies({
     } else {
       setSearchedMovies(moviesCardList)
     }
+// eslint-disable-next-line react-hooks/exhaustive-deps
   }, [moviesCardList])
 
   return (
